19c598d866df71116d7f64756800b2ec3af5b999
João Temochko <43001869+joaotemochko@users.noreply.github.com>
2024-02-25 09:34:25 -0300

    Adicionando arquivos do projeto

---
+++ app/javascript/admins_backoffice.js
@@ -0,0 +1,7 @@
+// Requisitos da aplicação (.js)
+//= require jquery/dist/jquery
+//= require jquery-ujs/src/rails
+//= require bootstrap/dist/js/bootstrap.bundle
+//= require jquery.easing/jquery.easing
+//= require sb-admin-2
+//
---
+++ app/javascript/users_backoffice.js
@@ -0,0 +1,9 @@
+// Import required js files
+//= require jquery/dist/jquery
+//= require jquery-ujs/src/rails
+//= require bootstrap/dist/js/bootstrap.bundle
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar
+//= require custom
+//
